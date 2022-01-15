0a3d671e13ee350a06a93d9fe1c6dabdc63c4914
Tomáš Kudělka <thorewi@gmail.com>
2022-01-15 15:53:57 +0100

    Update RouteList.php

--- src/RouteList.php
+++ src/RouteList.php
@@ -1,18 +1,25 @@
 <?php
 
 
-namespace App\Router;
+namespace ADT\Routing;
 
 
-use ADT\Routing\Route;
-
-class RouteList extends \ADT\Routing\RouteList
+class RouteList extends \Nette\Application\Routers\RouteList
 {
-	/** @var string|null */
-	public $path;
+	final public function addRoute(string $mask, $metadata = [], int $flags = 0)
+	{
+		$this->add(static::createRoute($mask, $metadata, $flags), $flags);
+		return $this;
+	}
+
+	final public function prependRoute(string $mask, $metadata = [], int $flags = 0)
+	{
+		$this->prepend(static::createRoute($mask, $metadata, $flags), $flags);
+		return $this;
+	}
 
 	public static function createRoute(string $mask, $metadata = [], int $flags = 0)
 	{
-		return new Route($mask, $metadata);
+		return new Route($mask, $metadata, $flags);
 	}
 }
