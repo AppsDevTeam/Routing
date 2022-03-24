c743061f7cd3ab9eb129b2558696ac9f8c557275
Tomáš Kudělka <thorewi@gmail.com>
2022-03-24 13:16:56 +0100

    Update Route.php

--- src/Route.php
+++ src/Route.php
@@ -2,29 +2,37 @@
 
 namespace ADT\Routing;
 
-use Nette\Application\Request as AppRequest;
+use Contributte\Translation\Translator;
 use Nette\Http\Url;
 use Nette\Http\UrlScript;
-use Nette\Localization\ITranslator;
+use Nette\Utils\Strings;
 
 
 /**
  * The bidirectional route with non-standard port support for absolute masks and locale
+ *
+ * / => [<locale=cs cs|en>]
+ * url => [<locale=cs cs|en>/]url
+ * /url => [<locale=cs cs|en>/]url
+ * https://www.example.com/url => https://www.example.com/[<locale=cs cs|en>/]url
  */
 class Route extends \Nette\Application\Routers\Route
 {
 	public bool $enableLocale;
 
-	public function __construct(string $mask, $metadata = [], int $flags = 0, $disableLocaleParameter = false, ?ITranslator $translator = null)
+	public function __construct(string $mask, $metadata = [], int $flags = 0, Translator $translator = null)
 	{
-		$this->enableLocale = !$disableLocaleParameter && $translator && count($translator->getAvailableLocales()) > 1;
+		$this->enableLocale = (bool) $translator;
 
 		if ($this->enableLocale) {
-			$locale = '[<locale' . ($translator->getDefaultLocale() ? '=' . $translator->getDefaultLocale() : '') . ' ' . implode('|', $translator->getAvailableLocales()) . '>/]';
 			$hostUrl = (new Url($mask))->getHostUrl();
-			$mask = $hostUrl ?
-				$hostUrl . '/' . ltrim(str_replace($hostUrl, $locale, $mask), '/'):
-				$locale . $mask;
+			$locale = '[<locale' . ($translator->getDefaultLocale() ? '=' . $translator->getDefaultLocale() : '') . ' ' . implode('|', $translator->getAvailableLocales()) . '>/]';
+
+			if ($hostUrl) {
+				$mask = str_replace($hostUrl . '/', $locale, $mask);
+			}
+			$mask = ltrim($mask, '/');
+			$mask = ($hostUrl ? $hostUrl . '/' : $locale) . $mask;
 		}
 
 		parent::__construct($mask, $metadata, $flags);
@@ -38,6 +46,15 @@ class Route extends \Nette\Application\Routers\Route
 			$url = new Url($url);
 			$url->setQueryParameter('originalLocale', null);
 			$url = (string)$url;
+
+			// removes trailing slash
+			if (
+				$url === $params['locale'] . '/' // en/
+				|| $url === '/' . $params['locale'] . '/' // /en/
+				|| Strings::endsWith($url, '/' . $params['locale'] . '/') // https://www.example.com/en/
+			) {
+				$url = rtrim($url, '/');
+			}
 		}
 
 		if ($url !== null && ! in_array($refUrl->getPort(), Url::$defaultPorts, true)) {
